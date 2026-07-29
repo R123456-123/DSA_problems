679b5edfe51d38adda040699a154768519775985
Rishiraj Tanwar <146092462+R123456-123@users.noreply.github.com>
2026-07-29 10:38:34 +0530

    Add: lemonade-change - submission-0

---
+++ Data Structures & Algorithms/lemonade-change/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean lemonadeChange(int[] bills) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(5, 0);
+        map.put(10, 0);
+        map.put(20, 0);
+
+        for (int bill : bills) {
+            if (bill == 10) {
+                if(map.get(5) > 0) {
+                    map.put(5, map.get(5) - 1);
+                } else {
+                    return false;
+                }
+                
+            } else if (bill == 20) {
+                if (map.get(5) > 0 && map.get(10) > 0) {
+                    map.put(5, map.get(5) - 1);
+                    map.put(10, map.get(10) - 1);
+                } else if (map.get(5) >= 3) {
+                    map.put(5, map.get(5) - 3);
+                } else {
+                    return false;
+                }
+
+            } else if (bill == 5) {
+                // do noting
+
+            } else {
+                return false;
+
+            }
+
+            map.put(bill, map.get(bill) + 1);
+        }
+
+        return true;
+    }
+}
